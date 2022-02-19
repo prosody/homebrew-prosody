671221fca60ed2db6dba36b759186189c8d2ac76
Kim Alvefur <zash@zash.se>
2022-02-19 17:29:53 +0100

    Update to Prosody 0.11.13 (closes #14)

--- prosody.rb
+++ prosody.rb
@@ -3,10 +3,12 @@ require "formula"
 class Prosody < Formula
   homepage "http://prosody.im"
 
-  url "https://prosody.im/downloads/source/prosody-0.9.14.tar.gz"
-  sha256 "27d1388acd79eaa453f2b194bd23c25121fe0a704d0dd940457caf1874ea1123"
-  version "0.9.14"
+  url "https://prosody.im/downloads/source/prosody-0.11.13.tar.gz"
+  sha256 "39c61b346a09b5125b604cb969e14206cbbcb86c81156ffc6ba2d62527cf0432"
+  version "0.11.13"
 
+  # NOTE Recommended version of Lua for Prosody 0.11.x is 5.2 but it is not
+  # available in Homebrew, while 5.1, 5.3 and 5.4 are
   depends_on "lua@5.1"
   depends_on "expat"
   depends_on "libidn"
@@ -43,12 +45,10 @@ class Prosody < Formula
 
     # patch config
     inreplace 'prosody.cfg.lua.install' do |s|
-      s.sub! '--"posix";', '"posix";'
       s.sub! 'info = "prosody.log";', "-- info = \"#{var}/log/prosody/prosody.log\";"
       s.sub! 'error = "prosody.err";', "-- error = \"#{var}/log/prosody/prosody.err\";"
       # s.sub! '-- "*syslog";', '"*syslog";'
       s.sub! '-- "*console";', '"*console";'
-      s.sub! '----------- Virtual hosts -----------', "daemonize=false\n\n----------- Virtual hosts -----------"
       # pid
     end
 
@@ -56,7 +56,7 @@ class Prosody < Formula
     system "luarocks", "install", "--tree=#{prefix}", "--lua-version=5.1", "luasocket"
     system "luarocks", "install", "--tree=#{prefix}", "--lua-version=5.1", "luasec", "OPENSSL_DIR=#{openssl.opt_prefix}"
     system "luarocks", "install", "--tree=#{prefix}", "--lua-version=5.1", "luaexpat", "EXPAT_DIR=#{expat.opt_prefix}"
-    system "luarocks", "install", "--tree=#{prefix}", "--lua-version=5.1", "bit32"
+    system "luarocks", "install", "--tree=#{prefix}", "--lua-version=5.1", "bit32" # NOTE only needed with Lua 5.1
 
     (etc+"prosody").mkpath
     (var+"lib/prosody").mkpath
@@ -82,7 +82,7 @@ class Prosody < Formula
   end
 
   test do
-    system "#{bin}/prosodyctl", "about"
+    system "#{bin}/prosodyctl", "check"
   end
 end
 
